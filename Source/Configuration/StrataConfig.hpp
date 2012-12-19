a716589400e17ddab95d7fa1df9246216bc2e6de
Bradley Clemetson <brad.clemetson@codeprogrammers.net>
2012-12-18 23:23:03 -0800

    *Started to make a Stargus Configration file

--- Source/Configuration/StrataConfig.hpp
+++ Source/Configuration/StrataConfig.hpp
@@ -1,3 +1,12 @@
+/*!
+ *  \brief     Singleton Class responsible for Config setup/system queries.
+ *  \details   
+ *  \author    Bradley Clemetson
+ *  \version   0.01
+ *  \date      December 18, 2012
+ *  \copyright GPLv2
+ */
+
 #ifndef StrataConfig_Header
 #define StrataConfig_Header
 
@@ -13,7 +22,20 @@ class StrataConfig
 {
     public:
         static StrataConfig *Configuration();
+    
+        //!Reads the Configuration XML
+        /*!  Reads the Configuration XML
+        *  \pre 
+        *  \post
+        *  \note*/
         void readConfig(boost::filesystem::path configurationPath);
+    
+        //!Queries the number of CPU Cores.
+        /*!  Queries the OS for the number of Core availible on
+         *  the CPU.
+         *  \pre None
+         *  \post Return the number of availible cores
+         *  \note*/
         static int GetCPUCores();
         float GetProgess();
     
