f95063c6ed21cd3142f8fa6be3fae529c3edaca7
Daniel Hodgson <toasty27@gmail.com>
2012-12-30 02:52:31 -0800

    Added a shell script for updating the name attribute in the XML config files

---
+++ Docs/CodeExamples/name.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+filename=$1
+search_string=$2
+replace_string=$3
+
+for value in $(cat $filename | fgrep $search_string | sed -e 's/\ \{2\}//' | sed -e 's/\ /\t/g' | cut -f 3 | cut -d \" -f 2)
+do
+	n=1
+	for i in `seq -w 0 10`
+	do
+		if echo $value | grep $i > /dev/null
+		then
+			xmlstarlet ed -u "/xml/audio[@path='$value']/@name" -v $replace_string$n $filename | diff $filename - | patch $filename -
+		else
+			n=`expr $n + 1`
+		fi
+	done
+done
