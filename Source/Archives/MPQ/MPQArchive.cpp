3fbb2544bb94ac3830336176fc206bf7d7affb98
Bradley Clemetson <brad.clemetson@codeprogrammers.net>
2012-12-30 04:32:48 -0800

    *Implemented basic MPQArchive methods

--- Source/Archives/MPQ/MPQArchive.cpp
+++ Source/Archives/MPQ/MPQArchive.cpp
@@ -1,11 +1,75 @@
 #include "MPQArchive.hpp"
 
+MPQArchive::MPQArchive()
+{
+    mpqArchive = NULL;
+}
+MPQArchive::~MPQArchive()
+{
+    
+}
+
+//Creates archive
+void MPQArchive::CreateArchive(boost::filesystem::path newArchivePath, int maxNumberOfFiles, DWORD MPQArchiveVersion)
+{
+    if(!SFileCreateArchive(newArchivePath.string().c_str(), MPQArchiveVersion, 1000, &mpqArchive))
+    {
+        throw "Failed to create archive";
+    }
+}
+
 void MPQArchive::OpenArchive(boost::filesystem::path archivePath)
 {
-    if (!SFileOpenArchive(archivePath.string().c_str(), 0, MPQ_OPEN_READ_ONLY, &mpqArchive))
+    if(mpqArchive)
+    {
+        throw "Archive already open";
+    }
+    if(!SFileOpenArchive(archivePath.string().c_str(), 0, 0, &mpqArchive))
     {
         std::cerr << "Failed to open the file '" << archivePath << "'\n";
         exit (-1);
+    }
+    
+}
+
+void MPQArchive::OpenArchive(boost::filesystem::path archivePath, boost::filesystem::path listPath)
+{
+    if(mpqArchive)
+    {
+        throw "Archive already open";
+    }
+    if(!SFileOpenArchive(archivePath.string().c_str(), MPQ_OPEN_NO_LISTFILE, MPQ_OPEN_FORCE_MPQ_V1, &mpqArchive))
+    {
+        std::cerr << "Failed to open the file '" << archivePath << "'\n";
+        exit (-1);
+    }
+    
+}
+
+void MPQArchive::ApplyListFile(boost::filesystem::path listFilePath)
+{
+    if(!mpqArchive)
+    {
+        throw "Archive not opened";
+    }
+    else
+    {
+        SFileAddListFile(mpqArchive, listFilePath.string().c_str());
+    }
+}
+
+void MPQArchive::ExtractRaw(boost::filesystem::path archiveFilePath, boost::filesystem::path destinationPath)
+{
+    if(!mpqArchive)
+    {
+        throw "Archive not opened";
+    }
+    else
+    {
+        if(!SFileExtractFile(&mpqArchive, archiveFilePath.string().c_str(), destinationPath.string().c_str(), SFILE_OPEN_FROM_MPQ))
+        {
+           throw "Failed to extract file";
+        }
     }
 }
 
--- Source/Archives/MPQ/MPQArchive.hpp
+++ Source/Archives/MPQ/MPQArchive.hpp
@@ -12,8 +12,11 @@ class MPQArchive
         MPQArchive();
         ~MPQArchive();
     
+        void CreateArchive(boost::filesystem::path newArchivePath, int maxNumberOfFiles, DWORD MPQArchiveVersion);
         void OpenArchive(boost::filesystem::path archivePath);
-        void CreateArchive(boost::filesystem::path newArchivePath);
+        void OpenArchive(boost::filesystem::path archivePath, boost::filesystem::path listPath);
+        void ApplyListFile(boost::filesystem::path listFilePath);
+        void ExtractRaw(boost::filesystem::path archiveFilePath, boost::filesystem::path destinationPath);
         void CloseArchive();
     protected:
     
