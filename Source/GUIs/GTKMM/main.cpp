e5a4e589dfe6323b6ce1e9257f92b8bf3ea10268
Bradley Clemetson <brad.clemetson@codeprogrammers.net>
2013-03-24 07:01:02 -0700

    *Changed the GTKMM example to be compatible with OSX and Linux

--- Source/GUIs/GTKMM/main.cpp
+++ Source/GUIs/GTKMM/main.cpp
@@ -1,62 +1,12 @@
-#ifndef GTKMM_EXAMPLE_HELLOWORLD_H
-#define GTKMM_EXAMPLE_HELLOWORLD_H
+#include <gtkmm.h>
 
-#include <gtkmm/button.h>
-#include <gtkmm/window.h>
-
-class HelloWorld : public Gtk::Window
+int main(int argc, char *argv[])
 {
-
-public:
-  HelloWorld();
-  virtual ~HelloWorld();
-
-protected:
-  //Signal handlers:
-  void on_button_clicked();
-
-  //Member widgets:
-  Gtk::Button m_button;
-};
-
-#endif // GTKMM_EXAMPLE_HELLOWORLD_H
-#include <iostream>
-
-HelloWorld::HelloWorld()
-: m_button("Hello World")   // creates a new button with label "Hello World".
-{
-  // Sets the border width of the window.
-  set_border_width(10);
-
-  // When the button receives the "clicked" signal, it will call the
-  // on_button_clicked() method defined below.
-  m_button.signal_clicked().connect(sigc::mem_fun(*this,
-              &HelloWorld::on_button_clicked));
-
-  // This packs the button into the Window (a container).
-  add(m_button);
-
-  // The final step is to display this newly created widget...
-  m_button.show();
-}
-
-HelloWorld::~HelloWorld()
-{
-}
-
-void HelloWorld::on_button_clicked()
-{
-  std::cout << "Hello World" << std::endl;
-}
-
-#include <gtkmm/application.h>
-
-int main (int argc, char *argv[])
-{
-  Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
-
-  HelloWorld helloworld;
-
-  //Shows the window and returns when it is closed.
-  return app->run(helloworld);
+    Gtk::Main kit(argc, argv);
+    
+    Gtk::Window window;
+    
+    Gtk::Main::run(window);
+    
+    return EXIT_SUCCESS;
 }
