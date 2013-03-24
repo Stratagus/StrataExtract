f13c16bd8714088229fe4814e4556b4cc60ef4d8
Bradley Clemetson <brad.clemetson@codeprogrammers.net>
2013-03-24 02:57:25 -0700

    *Making progress on adding a basic Quartz interface

---
+++ Source/GUIs/Quartz/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StarTool
+//
+//  Created by Bradley C. on 9/14/12.
+//  Copyright (c) 2012 Bradley Clemetson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
