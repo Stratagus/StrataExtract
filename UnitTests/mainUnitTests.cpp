d9a9976451cbfa2c0977d93992545a8650690c69
Bradley Clemetson <brad.clemetson@codeprogrammers.net>
2013-03-24 02:24:22 -0700

    *Added examples of unit tests

--- UnitTests/mainUnitTests.cpp
+++ UnitTests/mainUnitTests.cpp
@@ -1,7 +1,14 @@
-#include<iostream>
+#include "mainUnitTests.hpp"
 
 int main()
 {
-	std::cout << "Testing Hello World!\n";
+	std::cout << "StrataExtract Core Unit Tests\n";
+    
+    CppUnit::TextUi::TestRunner runner;
+    
+    //Main Object
+    runner.addTest( SampleUnitTest::suite() );
+    
+    runner.run();
 	return 0;
 }
---
+++ UnitTests/mainUnitTests.hpp
@@ -0,0 +1,21 @@
+/*!
+ *  \brief
+ *  \details
+ *  \author    Bradley Clemetson
+ *  \version   0.01
+ *  \date      March 24, 2013
+ *  \copyright GPLv2
+ */
+
+#ifndef Main_UnitTests_Header
+#define Main_UnitTests_Header
+
+//CPPUnit Test Includes
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/TestCase.h>
+#include <cppunit/TestCaller.h>
+
+#include "./Configuration/StrataConfigUnitTests.hpp"
+
+
+#endif
