3bb8f3f804d35a607323d5533e91db65a303bf70
Bradley Clemetson <brad.clemetson@codeprogrammers.net>
2013-06-30 18:02:45 -0700

    *Implemented essential bridge structure

--- Source/Conversion/Image/GRPImage/GRPImage.hpp
+++ Source/Conversion/Image/GRPImage/GRPImage.hpp
@@ -1,14 +1,18 @@
 #ifndef GRPImage_Header
 #define GRPImage_Header
 
-#include <Magick++.h>
+#include "../StrataImage.hpp"
 #include <boost/filesystem/path.hpp>
 
-class GRPImage
+class GRPImage : public StrataImage
 {
     public:
         GRPImage();
         ~GRPImage();
+        //virtual void ReadImageFromFile(boost::filesystem::path const &sourceImagePath);
+        //virtual void ReadImageFromMemory(std::vector<char> *image);
+        //virtual void WriteImageToFile(boost::filesystem::path const &destinationImagePath);
+        //virtual void ConvertImageFile(boost::filesystem::path const &sourceImagePath, boost::filesystem::path  const &destinationimagePath);
     protected:
     private:
 };
--- Source/Conversion/Image/StrataImage.hpp
+++ Source/Conversion/Image/StrataImage.hpp
@@ -1,15 +1,22 @@
 #ifndef StrataImage_Header
 #define StrataImage_Header
-#include "ImageMagick/MagickImage.hpp"
-#include "GRPImage/GRPImage.hpp"
+
+#include "StrataImageException.hpp"
+
+#include <boost/filesystem/path.hpp>
+
 
 
 class StrataImage
 {
     public:
-        StrataImage();
-        ~StrataImage();
+        virtual void ReadImageFromFile(boost::filesystem::path const &sourceImagePath) = 0;
+        virtual void ReadImageFromMemory(std::vector<char> *image) = 0;
+        virtual void WriteImageToFile(boost::filesystem::path const &destinationImagePath) = 0;
+        virtual void ConvertImageFile(boost::filesystem::path const &sourceImagePath, boost::filesystem::path const &destinationimagePath) = 0;
+    
     protected:
+        StrataImage();
     private:
 };
 
