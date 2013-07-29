2ba2bd4da394fbe04b72c93d12d2f431ce25f1c5
Bradley Clemetson <brad.clemetson@codeprogrammers.net>
2013-07-29 13:36:16 -0700

    *Added CD Paranoia

---
+++ Source/Conversion/Audio/CDParanoia/CDParanoia.hpp
@@ -0,0 +1,15 @@
+#ifndef CDParanoia_Header
+#define CDParanoia_Header
+
+
+#define private cdparanoia_private
+    #include <cdda_interface.h>
+#undef private
+class CDParanoia
+{
+public:
+    CDParanoia();
+    ~CDParanoia();
+};
+
+#endif
