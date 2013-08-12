36477a1f4d1ee755795973fb764ab5fee66bdb13
Bradley Clemetson <brad.clemetson@codeprogrammers.net>
2013-08-11 21:19:23 -0700

    *Temporarily remove cdparanoia due to compilation bugs on OSX

--- Source/Conversion/Audio/CDParanoia/CDParanoia.hpp
+++ Source/Conversion/Audio/CDParanoia/CDParanoia.hpp
@@ -1,15 +1,4 @@
 #ifndef CDParanoia_Header
 #define CDParanoia_Header
 
-
-#define private cdparanoia_private
-    #include <cdda_interface.h>
-#undef private
-class CDParanoia
-{
-public:
-    CDParanoia();
-    ~CDParanoia();
-};
-
 #endif
