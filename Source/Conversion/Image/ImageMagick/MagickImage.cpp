09b4b0c1f00acda4537d01b18b8c2641176df6ad
Daniel Hodgson <toasty27@gmail.com>
2015-05-17 00:36:36 -0700

    Changed raw pointers to unique pointers for MagickImage class

--- Source/Conversion/Image/ImageMagick/MagickImage.cpp
+++ Source/Conversion/Image/ImageMagick/MagickImage.cpp
@@ -1,20 +1,18 @@
 #include "MagickImage.hpp"
 MagickImage::MagickImage()
 {
-    graphicImage = nullptr;
+
 }
 MagickImage::~MagickImage()
 {
-    delete graphicImage;
-    graphicImage = nullptr;
+
 }
 
 void MagickImage::ReadImage(boost::filesystem::path const &sourceImagePath)
 {
-    if(graphicImage == nullptr)
-    {
-        graphicImage = new Magick::Image;
-    }
+    if(graphicImage)
+        graphicImage = std::make_unique<Magick::Image>();
+    
     graphicImage->read(sourceImagePath.string());
 }
 
@@ -22,10 +20,10 @@ void MagickImage::ReadImage(std::vector<char> *image)
 {
     Magick::Blob Blob;
     Blob.update(&image->at(0), image->size());
-    if(graphicImage == nullptr)
-    {
-        graphicImage = new Magick::Image;
-    }
+    
+    if(graphicImage)
+        graphicImage = std::make_unique<Magick::Image>();
+        
     graphicImage->read(Blob);
 }
 
@@ -40,10 +38,9 @@ void MagickImage::WriteImageToFile(boost::filesystem::path const &destinationIma
 }
 void MagickImage::ConvertImageFile(boost::filesystem::path const &sourceImagePath, boost::filesystem::path const &destinationimagePath)
 {
-    if(graphicImage == nullptr)
-    {
-        graphicImage = new Magick::Image;
-    }
+    if(graphicImage)
+        graphicImage = std::make_unique<Magick::Image>();
+
     graphicImage->read(sourceImagePath.string());
     graphicImage->write(destinationimagePath.string());
 }
